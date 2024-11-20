9693e27fbc54b2ab1e83895d88ba53b658ae8db3
Michael Spari <michaelspari72@gmail.com>
2024-11-20 14:29:58 +0100

    logic finish

---
+++ question.js
@@ -0,0 +1,146 @@
+let questions = [
+    {
+        "question": "Wie heißt die Hauptfigur des Buches?",
+        "answer_1": "Lina",
+        "answer_2": "Anna",
+        "answer_3": "Sophie",
+        "answer_4": "Mia",
+        "right_answer": 1
+    },
+    {
+        "question": "Was trägt Lina immer?",
+        "answer_1": "Ein blaues Kleid",
+        "answer_2": "Ein gelbes T-Shirt",
+        "answer_3": "Ein rotes Kleid",
+        "answer_4": "Eine grüne Jacke",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie heißt Linas bester Freund?",
+        "answer_1": "Max",
+        "answer_2": "Ali",
+        "answer_3": "Theo",
+        "answer_4": "Timo",
+        "right_answer": 2
+    },
+    {
+        "question": "Was bekommt Lina zur Einschulung?",
+        "answer_1": "Einen Schulranzen",
+        "answer_2": "Eine rote Schultüte",
+        "answer_3": "Einen Teddybären",
+        "answer_4": "Ein Fahrrad",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Tier hat Lina als Paten?",
+        "answer_1": "Ein Pferd",
+        "answer_2": "Ein Hund",
+        "answer_3": "Ein Kaninchen",
+        "answer_4": "Eine Katze",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie heißt Linas Geschwisterchen?",
+        "answer_1": "Leo",
+        "answer_2": "Lara",
+        "answer_3": "Luca",
+        "answer_4": "Lisa",
+        "right_answer": 1
+    },
+    {
+        "question": "Was lernt Lina von ihrem Opa Hermann?",
+        "answer_1": "Bienenpflege",
+        "answer_2": "Kochen",
+        "answer_3": "Malen",
+        "answer_4": "Lesen",
+        "right_answer": 1
+    },
+    {
+        "question": "Was macht Lina bei ihrer Fahrradprüfung?",
+        "answer_1": "Sie fährt durch einen Parcours",
+        "answer_2": "Sie malt ein Fahrrad",
+        "answer_3": "Sie hilft einem Freund",
+        "answer_4": "Sie lernt Verkehrsregeln",
+        "right_answer": 1
+    },
+    {
+        "question": "Was liebt Lina im Winter?",
+        "answer_1": "Schneemänner bauen",
+        "answer_2": "Schlittschuh laufen",
+        "answer_3": "Rodeln",
+        "answer_4": "Schneeballschlachten",
+        "right_answer": 1
+    },
+    {
+        "question": "Wo spielt Lina mit ihren Freunden ein Verkleidungsspiel?",
+        "answer_1": "Im Garten",
+        "answer_2": "In der Schule",
+        "answer_3": "Im Wald",
+        "answer_4": "Am See",
+        "right_answer": 1
+    },
+    {
+        "question": "Was verkauft Linas Opa auf dem Markt?",
+        "answer_1": "Gemüse",
+        "answer_2": "Honig",
+        "answer_3": "Brot",
+        "answer_4": "Blumen",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie hilft Lina beim Hochwasser?",
+        "answer_1": "Sie sammelt Spenden",
+        "answer_2": "Sie kocht Essen",
+        "answer_3": "Sie organisiert Spiele",
+        "answer_4": "Sie baut Häuser",
+        "right_answer": 1
+    },
+    {
+        "question": "Was lernt Lina beim Schwimmunterricht?",
+        "answer_1": "Wie man taucht",
+        "answer_2": "Wie man eine Freundin tröstet",
+        "answer_3": "Wie man einen Schwimmreifen benutzt",
+        "answer_4": "Wie man ein Sprungbrett benutzt",
+        "right_answer": 2
+    },
+    {
+        "question": "Was passiert im Kapitel mit dem Zug?",
+        "answer_1": "Lina sieht eine Kuh auf einer Wiese",
+        "answer_2": "Lina verliert ihr Ticket",
+        "answer_3": "Lina trifft einen Hund",
+        "answer_4": "Lina spielt ein Spiel",
+        "right_answer": 1
+    },
+    {
+        "question": "Was macht Lina auf dem Dorfmarkt?",
+        "answer_1": "Kauft Obst",
+        "answer_2": "Backt Brot",
+        "answer_3": "Singt ein Lied",
+        "answer_4": "Verkauft Kuchen",
+        "right_answer": 1
+    },
+    {
+        "question": "Warum isst Lina kein Fleisch mehr?",
+        "answer_1": "Wegen ihrer Gesundheit",
+        "answer_2": "Wegen der Tiere",
+        "answer_3": "Wegen ihrer Freunde",
+        "answer_4": "Wegen der Schule",
+        "right_answer": 2
+    },
+    {
+        "question": "Was lernt Lina im Dinosauriermuseum?",
+        "answer_1": "Die Größe eines Dinos",
+        "answer_2": "Die Namen der Dinos",
+        "answer_3": "Die Ernährung von Dinos",
+        "answer_4": "Die Geschichte der Dinos",
+        "right_answer": 4
+    },
+    {
+        "question": "Welcher Wert zieht sich durch das Buch?",
+        "answer_1": "Mut",
+        "answer_2": "Ehrlichkeit",
+        "answer_3": "Empathie",
+        "answer_4": "Wissen",
+        "right_answer": 3
+    }
+];
--- script.js
+++ script.js
@@ -58,6 +58,7 @@ let questions = [
 ];
 
 let currentQuestion = 0;
+let rightAnswers = 0;
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -70,7 +71,18 @@ function showQuestion() {
         document.getElementById('endScreen').style = '';
         document.getElementById('questionBody').style = 'display: none';
 
+        document.getElementById('amount-Of-questions').innerHTML = questions.length;
+        document.getElementById('amount-of-right-answers').innerHTML = rightAnswers;
+        document.getElementById('header-image').src = 'img/win.webp';
+
     } else {
+
+        let percent = (currentQuestion + 1) / questions.length;
+        percent = Math.round(percent * 100);
+        document.getElementById('progress-bar').innerHTML = `${percent}%`;
+        document.getElementById('progress-bar').style = `width: ${percent}%`;
+        console.log('Fotschritt: ', percent);
+
     let question = questions[currentQuestion];
     document.getElementById('question-number').innerHTML = currentQuestion + 1;
     document.getElementById('questionText').innerHTML = question['question'];
@@ -93,6 +105,7 @@ function answer(selection) {
     if(selectedQuestionNumber == question['right_answer']) {
         console.log('Richtige Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightAnswers++;
     }else { 
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
@@ -119,4 +132,14 @@ function resetAnswerButtons(){
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
+
+function restartGame(){
+    document.getElementById('header-image').src = 'img/quizTime.jpg';
+    document.getElementById('questionBody').style = '';
+    document.getElementById('endScreen').style = 'display: none';
+    currentQuestion = 0;
+    rightAnswers = 0;
+    init();
+}
+
     
